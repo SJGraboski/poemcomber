b2e455bab564cb4a1be9f9f4559abe74732d177a
Steven Graboski <sgrabosk@gmail.com>
2016-05-15 13:03:28 -0400

    changed registration rule to register students instead of instructor

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,33 +4,9 @@
 var jwt = require('jsonwebtoken');
 
 /* -/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/
-    FOR EVERY USER API ROUTE, YOU MUST CALL THIS FUNCTION
+    FOR EVERY USER API ROUTE, MAKE SURE YOU USE req.decoded
  * -/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/
-    It checks whether the user is logged in
-    and ensures that only instructors/students 
-    can access their assigned pages
- ======================================================== */ 
-function checkUser(callback){
-	// get the user who logged in
-	// 1: grab cookie (defined in auth-routes.js)
-	var token  = new Cookies(req, res).get('acces_token');
-
-	// now verify the token is a good one
-	jwt.verify(token, app.get('jwtsecret'), function(err, decoded){
-
-		// if you get an error, log the err
-		if (err){
-			console.log(err);
-			return false;
-		}
-		// if not grab the info we need
-		else{
-			console.log()
-			// place the decoded info in the callback
-			callback(decoded);
-		}
-	})
-}
+ This is the decoded cookie retrieved in auth-routes */
 
 // test route
 module.exports = function(app) {
--- routes/dry_attempt.js
+++ routes/dry_attempt.js
@@ -11,23 +11,44 @@ var User = require('../model/user.js');
 var Comment = require('../model/comments.js');
 var Assignment = require('../model/assignments.js');
 
+function login(result) {
+    var user = result[0].dataValues;
+    // create JSON token
+    var token = jwt.sign(user, app.get('jwtSecret'), {
+        expiresIn: 1440 // Token is given but will expire in 24 hours (requiring a re-login)
+    });
+    console.log(token);
+    var cookies = new Cookies(req, res).set('access_token', token, {
+        httpOnly: true,
+        secure: false
+    });
+    console.log(cookies);
+
+    // Then send it to the user. This token will need to be used to access the API
+    res.json({
+        success: true,
+        message: "Access granted. Proceed to the holy gateway of our API. Just be sure to use the token!",
+        token: token
+    });
+}
+
 // ===============================================================================
 // ROUTING
 // ===============================================================================
 
 module.exports = function(app){
 
-    // API POST Requests
-    // The code in here authenticates against the admin data. 
-    // If a user exists in the admin list they are given an access "token"
-    // Otherwise, they are denied access to the api route
-    // ---------------------------------------------------------------------------
+	// API POST Requests
+	// The code in here authenticates against the admin data. 
+	// If a user exists in the admin list they are given an access "token"
+	// Otherwise, they are denied access to the api route
+	// ---------------------------------------------------------------------------
 
     // login post
-    app.post('/api/login', function(req, res){
+	app.post('/api/login', function(req, res){
 
-        var username = req.body.username;
-        var password = req.body.password;
+		var username = req.body.username;
+		var password = req.body.password;
 
         // find user by searching for username and password
         User.findAll({
@@ -35,28 +56,12 @@ module.exports = function(app){
                 username: username,
                 password: password
             }
-        }).then(function(result){ // then save the result as the user obj
-            var user = result[0].dataValues;
-            // create JSON token
-            var token = jwt.sign(user, app.get('jwtSecret'), {
-                expiresIn: 1440 // Token is given but will expire in 24 hours (requiring a re-login)
-            });
-
-            new Cookies(req, res).set('access_token', token, {
-                httpOnly: true,
-                secure: false
-            });
-
-            // Then send it to the user. This token will need to be used to access the API
-            res.json({
-                success: true,
-                message: "Access granted. Proceed to the holy gateway of our API. Just be sure to use the token!",
-                token: token
-            });
+        }).then(function(result){
+            login(result);
         }).catch(function(err) {
             res.status(403).json("{'error':'" + err + "'");
         })
-    });
+	});
 
     // register post for students
     app.post('/api/register', function(req, res){
@@ -68,29 +73,16 @@ module.exports = function(app){
         User.create({
                 username: username,
                 password: password,
-                role: "student"
+                role: "instructor"
         }).then(function(){ // and we log them in upon registration, same as above
             User.findAll({
-            where: {
-                username: username,
-                password: password
-            }
+                where: {
+                    username: username,
+                    password: password
+                }
             })
         }).then(function(result){
-            var user = result[0].datavalues;
-            // create JSON token
-            var token = jwt.sign(user, app.get('jwtSecret'), {
-                expiresIn: 1440 // Token is given but will expire in 24 hours (requiring a re-login)
-            });
-
-            // send a cookie to the user with the proper info. 
-            new Cookies(req, res).set('access_token', token, {
-                httpOnly: false,
-                secure: false
-            });
-
-            // send response
-            res.send("{'message':'Success! You're in!'")
+            login(result);
         }).catch(function(err) {
             res.status(403).json("{'error':'" + err + "'");
         })
@@ -99,17 +91,17 @@ module.exports = function(app){
     // the all command
     app.all('*', function(req, res, next){
         var token = new Cookies(req, res).get('access_token');
-        console.log(token);
         // verify the token
         jwt.verify(token, app.get('jwtSecret'), function(err, decoded) {
             if (err) {
+                console.log(decoded);
                 console.log("Not yet");
                 return res.json({success: false, message: "access denied"})
             }
             else {
                 console.log("looks legit!")
-                req.decoded = decoded;
                 console.log(decoded);
+                req.decoded = decoded;
                 next();
             }
         })
