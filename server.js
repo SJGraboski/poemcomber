457b83f09cad4580c96b89118e3a173f89743584
Steven Graboski <sgrabosk@gmail.com>
2016-05-16 20:33:33 -0400

    sitemap support

--- server.js
+++ server.js
@@ -10,7 +10,13 @@ var jwt = require('jsonwebtoken');
 var path = require('path');
 var sm = require('sitemap')
 // Creates a sitemap object given the input configuration with URLs 
-var sitemap = sm.createSitemap({ options });
+var sitemap = sm.createSitemap({
+	hostname: 'http://example.com',
+      cacheTime: 600000,        // 600 sec - cache purge period 
+      urls: [
+        { url: '/',  changefreq: 'daily', priority: 1 }
+      ]
+    });
 // Generates XML with a callback function 
 sitemap.toXML( function(xml){ console.log(xml) });
 // Gives you a string containing the XML data 
